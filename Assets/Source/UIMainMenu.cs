c1226a87b2fe310e071fd2d98a29926251a78f59
Tomas Aquistapace <tomasaquistapace@pathfindersstudio.com>
2024-04-15 19:17:10 -0300

    add sound in menu

--- Assets/Source/UIMainMenu.cs
+++ Assets/Source/UIMainMenu.cs
@@ -1,27 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UIMainMenu : MonoBehaviour
 {
-    [SerializeField]
-    GameObject CreditsPanel;
-    [SerializeField]
-    GameObject ControlsPanel;
+    [SerializeField] private GameObject CreditsPanel = null;
+    [SerializeField] private GameObject ControlsPanel = null;
 
     public void GoToGameplayScene()
     {
         SceneManager.LoadScene("Gameplay");
     }
+
     public void SetCreditsPanelVisibility(bool IsVisible)
     {
         CreditsPanel.SetActive(IsVisible);
     }
+    
     public void SetControlsPanelVisibility(bool IsVisible)
     {
         ControlsPanel.SetActive(IsVisible);
     }
+    
     public void CloseGame()
     {
         Application.Quit();
