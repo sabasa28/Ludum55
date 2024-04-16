062e6089ece6b2ff5ef76870f8dfad8eebb4b339
Iñaki Diez Galarza <inakidiezgalarza@gmail.com>
2024-04-15 22:29:06 -0300

    Agregados otros sabores de rata

--- Assets/Source/RatSpawner.cs
+++ Assets/Source/RatSpawner.cs
@@ -12,6 +12,12 @@ public class RatSpawner : MonoBehaviour
     float TimeBetweenRatSpawns;
     [SerializeField]
     Rat RatPrefabToInstantiate;
+    [SerializeField]
+    Rat RedRatPrefabToInstantiate;
+    [SerializeField]
+    Rat ArmoredRatPrefabToInstantiate;
+    [SerializeField]
+    Rat RedArmoredRatPrefabToInstantiate;
     //a quien seguirian las ratas spawneadas, es decir el player
     public Entity EntityToTarget;
 
@@ -33,7 +39,31 @@ public class RatSpawner : MonoBehaviour
         transform.localScale = Vector3.one;
         for (int i = 0; i < RatsToSpawn; i++)
         {
-            Rat RatSpawned = Instantiate<Rat>(RatPrefabToInstantiate, transform.position, Quaternion.identity);
+            int Rand = Random.Range(0, 8);
+            Rat Prefab;
+            switch (Rand) //perdoname dios pues he pecado
+            {
+                case 0:
+                case 1:
+                case 2:
+                    Prefab = RatPrefabToInstantiate;
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Prefab = RedRatPrefabToInstantiate;
+                    break;
+                case 6:
+                    Prefab = ArmoredRatPrefabToInstantiate;
+                    break;
+                case 7:
+                    Prefab = RedArmoredRatPrefabToInstantiate;
+                    break;
+                default:
+                    Prefab = RatPrefabToInstantiate;
+                    break;
+            }
+            Rat RatSpawned = Instantiate<Rat>(Prefab, transform.position, Quaternion.identity);
             RatSpawned.EntityToChase = EntityToTarget;
             yield return new WaitForSeconds(TimeBetweenRatSpawns);
         }
